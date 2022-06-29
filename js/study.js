ab828ddfc59e5c57a0044b15f9100b8d03bc3b67
RodionShpak <rodion.it22@gmail.com>
2022-06-29 21:43:10 +0300

    задача

--- js/study.js
+++ js/study.js
@@ -569,3 +569,19 @@
 //     // Change code above this line
 //   },
 // };
+
+
+const forecast = {
+    today: {
+        low: 28,
+        high: 32,
+        icon: "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
+    },
+    tomorrow: {
+        low: 27,
+        high: 31,
+    },
+};
+// Change code below this line
+
+const { today: { high: highToday, low: lowToday, icon: todayIcon = "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg" }, tomorroW: { high: highTomorrow, low: lowTomorrow, icon: tomorrowIcon = "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg" } } = forecast;
