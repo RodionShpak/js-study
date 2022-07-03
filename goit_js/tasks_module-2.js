c1855f8b3e77184efcf6f99c57d04d678a02fb3a
RodionShpak <rodion.it22@gmail.com>
2022-07-03 17:39:57 +0300

    task 11

--- goit_js/tasks_module-2.js
+++ goit_js/tasks_module-2.js
@@ -331,7 +331,31 @@
 //     return message.split(delimeter);
 // }
 
+// ###########################################################################################################################################################
 
+// // ==========================================Task-11
+// ЗАДАЧА: ГРАВИРОВКА УКРАШЕНИЙ
+
+// //  --------------------===================== Сonditions --------------------=====================
+// Сервису гравировки украшений нужна функция, которая бы автоматически считала цену гравировки, в зависимости от количества слов и цены за слово.
+// Объявлена функция calculateEngravingPrice(message, pricePerWord).
+// Эта функция принимает строку, состоящую из слов разделённых только пробелами(параметр message)
+// и цену гравировки одного слова(параметр pricePerWord).
+// Напиши тело функции, чтобы она возвращала общую стоимость гравировки всех слов в строке.
+
+// //  --------------------===================== Test --------------------=====================
+// Объявлена функция calculateEngravingPrice(message, pricePerWord)
+// Вызов calculateEngravingPrice("JavaScript is in my blood", 10) возвращает 50
+// Вызов calculateEngravingPrice("JavaScript is in my blood", 20) возвращает 100
+// Вызов calculateEngravingPrice("Web-development is creative work", 40) возвращает 160
+// Вызов calculateEngravingPrice("Web-development is creative work", 20) возвращает 80
+
+// // --------------------===================== Result --------------------=====================
+// function calculateEngravingPrice(message, pricePerWord) {
+//     let newPrice = message.split(" ");
+//     let priceLenght = newPrice.length;
+//     return priceLenght * pricePerWord;
+// }
 
 
 
