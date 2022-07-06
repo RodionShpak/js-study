6134143d2e6814d2a27a110f3bd0deba6a82b01b
RodionShpak <rodion.it22@gmail.com>
2022-07-06 19:09:26 +0300

    апк

--- goit_js/tasks_module-1.js
+++ goit_js/tasks_module-1.js
@@ -1557,6 +1557,404 @@
 
 
 
+// ###########################################################################################################################################################
+
+// // --------------------===================== TASKS --------------------=====================
+// --------------------------Задача 1------------------------------------
+// Change code below this line
+// const productName = 'Droid';
+// console.log(productName);
+// 'Droid'
+// const pricePerItem = 2000;
+// console.log(pricePerItem);
+// 2000
+
+// --------------------------Задача 2------------------------------------
+// let productName = 'Droid';
+// productName = 'Repair droid';
+// let pricePerItem = 2000;
+// pricePerItem += 1500;
+
+// console.log(productName);
+// console.log(pricePerItem);
+
+// --------------------------Задача 3------------------------------------
+// const topSpeed = 160;
+// const distance = 617.54;
+// const login = 'mango935';
+// const isOnline = true;
+// const isAdmin = false;
+
+// console.log(topSpeed);
+// console.log(distance);
+// console.log(login);
+// console.log(isOnline);
+// console.log(isAdmin);
+
+// --------------------------Задача 4------------------------------------
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+// const totalPrice = pricePerItem * orderedQuantity;
+// console.log(totalPrice);
+
+// --------------------------Задача 5------------------------------------
+// const productName = 'Droid';
+// const pricePerItem = 3500;
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+// console.log(message);
+
+// --------------------------Задача 6------------------------------------
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+// console.log(message);
+
+// --------------------------Задача 7------------------------------------
+// function sayHi() {
+//   console.log('Hello, this is my first function!');
+// }
+// sayHi();
+
+// --------------------------Задача 8------------------------------------
+// function add(a, b, c) {
+//   console.log(`Addition result equals ${a + b + c}`);
+// }
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
+
+// --------------------------Задача 9------------------------------------
+// function add(a, b, c) {
+//   return a + b + c;
+// }
+
+// let result = add(2, 5, 8); // 15
+// console.log(result);
+
+// console.log(add(15, 27, 10));
+// console.log(add(10, 20, 30));
+// console.log(add(5, 10, 15));
+
+// --------------------------Задача 10------------------------------------
+// function makeMessage(name, price) {
+//   const message = `You picked ${name}, price per item is ${price} credits`;
+//   return message;
+// }
+// без виклику коду
+
+// --------------------------Задача 11------------------------------------
+// function calculateTotalPrice(orderedQuantity, pricePerItem) {
+//   const totalPrice = pricePerItem * orderedQuantity;
+//   return totalPrice;
+// }
+// без виклику коду
+
+// --------------------------Задача 12------------------------------------
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+//   const totalPrice = orderedQuantity * pricePerDroid + deliveryFee;
+//   const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+//   return message;
+// }
+// без виклику коду
+
+// --------------------------Задача 13------------------------------------
+// function isAdult(age) {
+//   const passed = age >= 18;
+//   return passed;
+// }
+
+// --------------------------Задача 14------------------------------------
+// function isValidPassword(password) {
+//   const SAVED_PASSWORD = 'jqueryismyjam';
+//   const isMatch = SAVED_PASSWORD === password;
+//   return isMatch;
+// }
+// --------------------------Задача 15------------------------------------
+// function checkAge(age) {
+//   let message;
+
+//   if (age >= 18) {
+//     message = 'You are an adult';
+//   } else {
+//     message = 'You are a minor';
+//   }
+//   return message;
+// }
+// --------------------------Задача 16------------------------------------
+// function checkStorage(available, ordered) {
+//   let message;
+
+//   if (available < ordered) {
+//     message = 'Not enough goods in stock!';
+//   } else {
+//     message = 'Order is processed, our manager will contact you.';
+//   }
+
+//   return message;
+// }
+// --------------------------Задача 17------------------------------------
+// let a = 5;
+// let b = 10;
+// let c = 15;
+// let d = 20;
+
+// a += 2;
+// b -= 4;
+// c *= 3;
+// d /= 10;
+
+// --------------------------Задача 18------------------------------------
+// function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+//   let message;
+//   // Change code below this line
+//   let totalPrice;
+//   totalPrice = pricePerDroid * orderedQuantity;
+
+//   if (totalPrice > customerCredits) {
+//     message = 'Insufficient funds!';
+//   } else {
+//     customerCredits -= totalPrice;
+//     message = `You ordered ${orderedQuantity} droids, you have ${customerCredits} credits left`;
+//   }
+//   // Change code above this line
+//   return message;
+// }
+
+// --------------------------Задача 19------------------------------------
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = 'jqueryismyjam';
+//   let message;
+
+//   if (password === null) {
+//     message = 'Canceled by user!';
+//   } else if (password === ADMIN_PASSWORD) {
+//     message = 'Welcome!';
+//   } else {
+//     message = 'Access denied, wrong password!';
+//   }
+
+//   return message;
+// }
+// --------------------------Задача 20------------------------------------
+// function checkStorage(available, ordered) {
+//   let message;
+//   // Change code below this line
+//   if (ordered === 0) {
+//     message = 'There are no products in the order!';
+//   } else if (available < ordered) {
+//     message = 'Your order is too large, there are not enough items in stock!';
+//   } else {
+//     message = 'The order is accepted, our manager will contact you';
+//   }
+//   // Change code above this line
+//   return message;
+// }
+// --------------------------Задача 21------------------------------------
+// function isNumberInRange(start, end, number) {
+//   const isInRange = number >= start && number <= end; // Change this line
+//   return isInRange;
+// }
+
+// --------------------------Задача 22------------------------------------
+// function checkIfCanAccessContent(subType) {
+//   const canAccessContent = subType === 'pro' || subType === 'vip'; // Change this line
+//   return canAccessContent;
+// }
+
+// --------------------------Задача 23------------------------------------
+// function isNumberNotInRange(start, end, number) {
+//   const isInRange = number >= start && number <= end;
+//   const isNotInRange = !isInRange; // Change this line
+//   return isNotInRange;
+// }
+// --------------------------Задача 24------------------------------------
+
+// if (totalSpent >= 50000) {
+//   discount = GOLD_DISCOUNT;
+// } else if (totalSpent >= 20000 && totalSpent < 50000) {
+//   discount = SILVER_DISCOUNT;
+// } else if (totalSpent >= 5000 && totalSpent < 20000) {
+//   discount = BRONZE_DISCOUNT;
+// } else {
+//   discount = BASE_DISCOUNT;
+// }
+
+// if (totalSpent < 5000) {
+//   discount = BASE_DISCOUNT;
+// } else if (totalSpent >= 5000 && totalSpent < 20000) {
+//   discount = BRONZE_DISCOUNT;
+// } else if (totalSpent >= 20000 && totalSpent < 50000) {
+//   discount = SILVER_DISCOUNT;
+// } else {
+//   discount = GOLD_DISCOUNT;
+// }
+
+// --------------------------Задача 25------------------------------------
+// function checkStorage(available, ordered) {
+//   let message;
+//   message =
+//     ordered > available
+//       ? 'Not enough goods in stock!'
+//       : 'The order is accepted, our manager will contact you';
+//   return message;
+// }
+
+// --------------------------Задача 26------------------------------------
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = 'jqueryismyjam';
+//   let message;
+//   // Change code below this line
+//   message = password === ADMIN_PASSWORD ? 'Access is allowed' : 'Access denied, wrong password!';
+//   // Change code above this line
+//   return message;
+// }
+
+// --------------------------Задача 27------------------------------------
+// function getSubscriptionPrice(type) {
+//   let price;
+
+//   switch (type) {
+//     case 'starter':
+//       price = 0;
+//       break;
+//     case 'professional':
+//       price = 20;
+//       break;
+//     case 'organization':
+//       price = 50;
+//       break;
+//   }
+
+//   return price;
+// }
+
+// --------------------------Задача 28------------------------------------
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = 'jqueryismyjam';
+//   let message;
+
+//   if (password === null) {
+//     message = 'Canceled by user!';
+//   } else if (password === ADMIN_PASSWORD) {
+//     message = 'Welcome!';
+//   } else {
+//     message = 'Access denied, wrong password!';
+//   }
+//   return message;
+// }
+
+// Рефакторинг коду
+// switch (password) {
+//   case null:
+//     message = 'Canceled by user!';
+//     break;
+//   case ADMIN_PASSWORD:
+//     message = 'Welcome!';
+//     break;
+//   default:
+//     message = 'Access denied, wrong password!';
+// }
+
+// --------------------------Задача 29------------------------------------
+// function getShippingCost(country) {
+//   let message;
+
+//   switch (country) {
+//     case 'China':
+//       message = `Shipping to ${country} will cost 100 credits`;
+//       break;
+
+//     case 'Chile':
+//       message = `Shipping to ${country} will cost 250 credits`;
+//       break;
+
+//     case 'Australia':
+//       message = `Shipping to ${country} will cost 170 credits`;
+//       break;
+
+//     case 'Jamaica':
+//       message = `Shipping to ${country} will cost 120 credits`;
+//       break;
+
+//     default:
+//       message = 'Sorry, there is no delivery to your country';
+//   }
+
+//   return message;
+// }
+// --------------------------Задача 30------------------------------------
+// function getNameLength(name) {
+//   const message = `Name ${name} is ${name.length} characters long`; // Change this line
+//   return message;
+// }
+// --------------------------Задача 31------------------------------------
+// const courseTopic = 'JavaScript essentials';
+
+// const courseTopicLength = courseTopic.length;
+// const firstElement = courseTopic[0];
+// const lastElement = courseTopic[courseTopicLength - 1]; //последний элемент строки
+
+// --------------------------Задача 32------------------------------------
+// function getSubstring(string, length) {
+//   const substring = string.slice(0, length); // Change this line
+
+//   return substring;
+// }
+// --------------------------Задача 33------------------------------------
+// function formatMessage(message, maxLength) {
+//   let result;
+//   if (message.length > maxLength) {
+//     result = message.slice(0, maxLength) + '...';
+//   } else {
+//     result = message;
+//   }
+//   return result;
+// }
+
+// --------------------------Задача 34------------------------------------
+// function normalizeInput(input) {
+//   const normalizedInput = input.toLowerCase(); // Change this line
+//   return normalizedInput;
+// }
+
+// --------------------------Задача 35------------------------------------
+// function checkForName(fullName, name) {
+//   const result = fullName.includes(name); // Change this line
+//   return result;
+// }
+// --------------------------Задача 36------------------------------------
+// function checkForSpam(message) {
+//   let result;
+//   // Change code below this line
+//   const testWord1 = 'spam';
+//   const testWord2 = 'sale';
+
+//   result = message.toLowerCase().includes(testWord1) || message.toLowerCase().includes(testWord2);
+
+//   // Change code above this line
+//   return result;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- goit_js/tasks_module-2.js
+++ goit_js/tasks_module-2.js
@@ -576,6 +576,425 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ###########################################################################################################################################################
+// //  --------------------===================== TASKS --------------------=====================
+
+// ------------------------------Задача 1------------------------------------
+// function checkAge(age) {
+//   if (age >= 18) {
+//     // Change this line
+//     return 'You are an adult';
+//   }
+
+//   return 'You are a minor';
+// }
+// console.log(checkAge(15));
+
+// ------------------------------Задача 2------------------------------------
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = 'jqueryismyjam';
+//   // Change code below this line
+
+//   if (password === ADMIN_PASSWORD) {
+//     return 'Welcome!';
+//   }
+//   return 'Access denied, wrong password!';
+// }
+
+// console.log(checkPassword('jqueryismyjam'));
+
+// ------------------------------Задача 3------------------------------------
+// function checkStorage(available, ordered) {
+
+//   if (ordered === 0) {
+//     return 'Your order is empty!';
+//   }
+//   if (ordered > available) {
+//     return 'Your order is too large, not enough goods in stock!';
+//   }
+//   return 'The order is accepted, our manager will contact you';
+// }
+
+// console.log(checkStorage(2000, 100));
+
+// ------------------------------Задача 4------------------------------------
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+// ------------------------------Задача 5------------------------------------
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+// const firstElement = fruits[0];
+// const secondElement = fruits[1];
+// const lastElement = fruits[fruits.length - 1];
+
+// console.log(lastElement); //orange
+
+// ------------------------------Задача 6------------------------------------
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+// fruits[1] = 'peach';
+// fruits[3] = 'banana';
+
+// console.log(fruits); // ['apple', 'peach', 'pear', 'banana']
+
+// ------------------------------Задача 7------------------------------------
+// const fruits = ['apple', 'peach', 'pear', 'banana'];
+
+// const fruitsArrayLength = fruits.length;
+// console.log(fruitsArrayLength); // 4
+
+// ------------------------------Задача 8------------------------------------
+// const fruits = ['apple', 'peach', 'pear', 'banana'];
+
+// const lastElementIndex = fruits.length - 1;
+// const lastElement = fruits[lastElementIndex];
+
+// console.log(lastElementIndex); //3
+// console.log(lastElement); //banana
+
+// ------------------------------Задача 9------------------------------------
+// function getExtremeElements(array) {
+//   const arrayLength = array.length;
+
+//   const arrayPartDel = arrayLength - 2;
+
+//   array.splice(1, arrayPartDel);
+//   return array;
+// }
+// console.log(getExtremeElements([1, 2, 3, 4, 5]));
+// console.log(getExtremeElements(['Earth', 'Mars', 'Venus']));
+// console.log(getExtremeElements(['apple', 'peach', 'pear', 'banana']));
+
+// ------------------------------Задача 10------------------------------------
+// function splitMessage(message, delimeter) {
+//   let words;
+
+//   words = message.split(delimeter);
+
+//   return words;
+// }
+
+// console.log(splitMessage('Mango hurries to the train', ' ')); // ["Mango", "hurries", "to", "the", "train"]
+// console.log(splitMessage('Mango', '')); // ["M", "a", "n", "g", "o"]
+// console.log(splitMessage('best_for_week', '_')); // ["best", "for", "week"]
+
+// ------------------------------Задача 11------------------------------------
+// function calculateEngravingPrice(message, pricePerWord) {
+//   return message.split(' ').length * pricePerWord;
+// }
+// console.log(calculateEngravingPrice('JavaScript is in my blood', 10)); // 50
+// console.log(calculateEngravingPrice('JavaScript is in my blood', 20)); // 100
+// console.log(calculateEngravingPrice('Web-development is creative work', 40)); // 160
+// console.log(calculateEngravingPrice('Web-development is creative work', 20)); // 80
+
+// ------------------------------Задача 12------------------------------------
+// function makeStringFromArray(array, delimeter) {
+//   let string;
+
+//   string = array.join(delimeter);
+
+//   return string;
+// }
+// console.log(makeStringFromArray(['Mango', 'hurries', 'to', 'the', 'train'], ' ')); // "Mango hurries to the train"
+// console.log(makeStringFromArray(['M', 'a', 'n', 'g', 'o'], '')); // "Mango"
+// console.log(makeStringFromArray(['top', 'picks', 'for', 'you'], '_')); // "top_picks_for_you"
+
+// ------------------------------Задача 13------------------------------------
+// function slugify(title) {
+//   return title.toLowerCase().split(' ').join('-');
+// }
+
+// console.log(slugify('Arrays for begginers'));
+// ------------------------------Задача 14------------------------------------
+// const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
+
+// const firstTwoEls = fruits.slice(0, 2);
+// const nonExtremeEls = fruits.slice(1, [fruits.length - 1]);
+// const lastThreeEls = fruits.slice(-3);
+
+// console.log(nonExtremeEls); // ['plum', 'pear', 'orange']
+
+// ------------------------------Задача 15------------------------------------
+// function makeArray(firstArray, secondArray, maxLength) {
+
+//   const allArray = firstArray.concat(secondArray);
+
+//   if (allArray.length > maxLength) {
+//     return allArray.slice(0, maxLength);
+//   }
+//   return allArray;
+// }
+// console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ['Mango', 'Poly', 'Ajax']
+// console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ['Mango', 'Poly', 'Houston', 'Ajax']
+// console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ['Mango', 'Ajax', 'Chelsea']
+// console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
+
+// ------------------------------Задача 16------------------------------------
+// const start = 3;
+// const end = 7;
+
+// for (let i = start; i <= end; i += 1) {
+//   console.log(i);
+// }
+
+// ------------------------------Задача 17------------------------------------
+// function calculateTotal(number) {
+//   let total = 0;
+//   for (let i = 1; i <= number; i += 1) {
+//     total += i;
+//   }
+//   return total;
+// }
+
+// console.log(calculateTotal(1)); //1
+// console.log(calculateTotal(3)); // 6
+// console.log(calculateTotal(7)); // 28
+
+// ------------------------------Задача 18------------------------------------
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+// for (let i = 0; i < fruits.length; i += 1) {
+//   const fruit = fruits[i];
+//   console.log(fruit);
+// }
+
+// ------------------------------Задача 19------------------------------------
+// function calculateTotalPrice(order) {
+//   let total = 0;
+
+//   for (let i = 0; i < order.length; i += 1) {
+//     total += order[i];
+//   }
+
+//   return total;
+// }
+
+// console.log(calculateTotalPrice([12, 85, 37, 4])); // 138
+// console.log(calculateTotalPrice([164, 48, 291])); // 503
+
+// ------------------------------Задача 20------------------------------------
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   // Change code below this line
+//   for (let i = 0; i < order.length; i += 1) {
+//     total += order[i];
+//   }
+//   // Change code above this line
+//   return total;
+// }
+// ------------------------------Задача 21------------------------------------
+// function findLongestWord(string) {
+//
+//   const arr = string.split(' ');
+//   // console.log(arr); // ['The', 'quick', 'brown', 'fox', 'jumped', 'over', 'the', 'lazy', 'dog']
+//   let wordMaxLength = arr[0];
+//   // console.log(wordMaxLength);
+//   for (const word of arr) {
+//     // console.log(word);
+//     if (word.length > wordMaxLength.length) {
+//       wordMaxLength = word;
+//       // console.log(word);
+//     }
+//     wordMaxLength = wordMaxLength;
+//   }
+//   return wordMaxLength;
+//   // Change code above this line
+// }
+
+// console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // jumped
+// console.log(findLongestWord('Google do a roll')); // Google
+// console.log(findLongestWord('May the force be with you')); // force
+
+// ------------------------------Задача 22------------------------------------
+// function createArrayOfNumbers(min, max) {
+//   const numbers = [];
+//   // Change code below this line
+//   // numbers.push(min);
+//   // console.log(numbers);
+//   for (let i = min; i <= max; i += 1) {
+//     numbers.push(i);
+//     // console.log(numbers);
+//   }
+//   // Change code above this line
+//   return numbers;
+// }
+// console.log(createArrayOfNumbers(1, 3)); // [1, 2, 3]
+// console.log(createArrayOfNumbers(14, 17)); // [14, 15, 16, 17]
+// console.log(createArrayOfNumbers(29, 34)); // [29, 30, 31, 32, 33, 34]
+
+// ------------------------------Задача 23------------------------------------
+// function filterArray(numbers, value) {
+
+//   const newArray = [];
+
+//   for (const number of numbers) {
+//     if (number > value) {
+//       newArray.push(number);
+//     }
+//   }
+//   return newArray;
+// }
+// console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+// console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+// console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+
+// ------------------------------Задача 24------------------------------------
+// function checkFruit(fruit) {
+//   const fruits = ['apple', 'plum', 'pear', 'orange'];
+//   return fruits.includes(fruit);
+// }
+
+// ------------------------------Задача 25------------------------------------
+// function getCommonElements(array1, array2) {
+//   // Change code below this line
+//   const array3 = [];
+
+//   for (const arr of array1) {
+//     console.log(arr);
+
+//     if (array1.includes(arr) && array2.includes(arr)) {
+//       array3.push(arr);
+//     }
+//   }
+//   return array3;
+//   // Change code above this line
+// }
+// console.log(getCommonElements([1, 2, 3], [2, 4])); // [2]
+// console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27])); // [12, 27, 3]
+// console.log(getCommonElements([1, 2, 3], [10, 20, 30])); // []
+
+// ------------------------------Задача 26------------------------------------
+// function calculateTotalPrice(order) {
+//   let total = 0;
+
+//   for (const element of order) {
+//     total += element;
+//   }
+
+//   return total;
+// }
+
+// ------------------------------Задача 27------------------------------------
+// function filterArray(numbers, value) {
+//   const filteredNumbers = [];
+
+//   for (const number of numbers) {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   }
+//   return filteredNumbers;
+// }
+
+// ------------------------------Задача 28------------------------------------
+// const a = 3 % 1;
+// const b = 4 % 3;
+// const c = 11 % 8;
+// const d = 12 % 7;
+// const e = 8 % 3;
+
+// ------------------------------Задача 29------------------------------------
+// function getEvenNumbers(start, end) {
+//   let array = [];
+//   for (let i = start; i <= end; i += 1) {
+//     if (i % 2 === 0) {
+//       array.push(i);
+//     }
+//   }
+//   return array;
+// }
+
+// console.log(getEvenNumbers(2, 5)); // [2, 4]
+// console.log(getEvenNumbers(3, 11)); // [4, 6, 8, 10]
+// console.log(getEvenNumbers(6, 12)); // [6, 8, 10, 12]
+// console.log(getEvenNumbers(8, 8)); // [8]
+
+// ------------------------------Задача 30------------------------------------
+// const start = 6;
+// const end = 27;
+// let number;
+
+// for (let i = start; i < end; i += 1) {
+//   if (i % 5 === 0) {
+//     number = i;
+//     break;
+//   }
+// }
+
+// ------------------------------Задача 31------------------------------------
+// function findNumber(start, end, divisor) {
+//   // Change code below this line
+//   let number;
+
+//   for (let i = start; i < end; i += 1) {
+//     if (i % divisor === 0) {
+//       number = i;
+//       return number;
+//     }
+//   }
+
+//   // Change code above this line
+// }
+
+// ------------------------------Задача 32------------------------------------
+// function includes(array, value) {
+//   for (const arr of array) {
+//     console.log(arr);
+//     if (arr === value) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+// console.log(includes([1, 2, 3, 4, 5], 3)); // [true]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // ###########################################################################################################################################################
 
 // // ==========================================Task-
