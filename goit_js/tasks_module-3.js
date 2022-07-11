985ddee526f9910dbe886b7932c5c3d6745bdc6c
RodionShpak <98263512+RodionShpak@users.noreply.github.com>
2022-07-11 15:27:48 +0300

    task 20

--- goit_js/tasks_module-3.js
+++ goit_js/tasks_module-3.js
@@ -1090,6 +1090,321 @@
 
 // // ==========================================Task-15
 
+// МЕТОД OBJECT.VALUES()
+// Якщо метод Object.keys(obj) повертає масив ключів власних властивостей об'єкта,
+// то метод Object.values(obj) повертає масив значень його власних властивостей.
+// Якщо в об'єкті відсутні властивості, метод Object.values(obj) поверне порожній масив.
+
+// const book = {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+// };
+// const keys = Object.keys(book);
+// console.log(keys); // ["title", "author", "genres", "rating"]
+
+// const values = Object.values(book);
+// console.log(values); // ["The Last Kingdom", "Bernard Cornwell", 8.38]
+// Масив значень властивостей також можна перебрати циклом for...of, наприклад для отримання загальної суми числових значень.
+
+// //  --------------------===================== Сonditions =====================--------------------
+
+// Запиши у змінну keys масив ключів власних властивостей об'єкта apartment, а у змінну values - масив всіх значень його властивостей.
+// Використовуй методи Object.keys() і Object.values().
+
+// //  --------------------===================== Tests =====================--------------------
+
+// Оголошена змінна apartment
+// Значення змінної apartment - це об'єкт
+// Оголошена змінна keys
+// Значення змінної keys - це масив["descr", "rating", "price"]
+// Оголошена змінна values
+// Значення змінної values - це масив["Spacious apartment in the city center", 4, 2153]
+// Для отримання масиву ключів об'єкта apartment використовується Object.keys()
+// Для отримання масиву значень об'єкта apartment використовується Object.values()
+
+// //  --------------------===================== Result =====================--------------------
+
+// // Before
+
+// const apartment = {
+//     descr: "Spacious apartment in the city center",
+//     rating: 4,
+//     price: 2153,
+// };
+// // Change code below this line
+// const keys = apartment;
+// const values = apartment;
+
+// // After
+
+// const apartment = {
+//     descr: "Spacious apartment in the city center",
+//     rating: 4,
+//     price: 2153,
+// };
+// let values = Object.values(apartment);
+// const keys = Object.keys(apartment);
+
+
+
+// ###########################################################################################################################################################
+
+// // ==========================================Task-16
+// ЗАДАЧА: ВИТРАТИ НА ЗАРПЛАТУ
+
+// //  --------------------===================== Сonditions =====================--------------------
+
+// Напиши функцію countTotalSalary(salaries), яка приймає об'єкт зарплат,
+// де ім'я властивості - це ім'я співробітника, а значення властивості - це зарплата.
+// Функція повинна розрахувати загальну суму зарплат співробітників і повернути її.
+// Використовуй змінну totalSalary для зберігання загальної суми зарплати.
+
+// //  --------------------===================== Tests =====================--------------------
+
+// Оголошена функція countTotalSalary(salaries)
+// Виклик countTotalSalary({}) повертає 0
+// Виклик countTotalSalary({ mango: 100, poly: 150, alfred: 80 }) повертає 330
+// Виклик countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }) повертає 400
+// Функція враховує тільки власні властивості об'єкта
+
+// //  --------------------===================== Result =====================--------------------
+
+// // Before
+
+// function countTotalSalary(salaries) {
+//     let totalSalary = 0;
+//     // Change code below this line
+
+//     // Change code above this line
+//     return totalSalary;
+// }
+
+// // After
+
+// function countTotalSalary(salaries) {
+//     let totalSalary = 0;
+//     const values = Object.values(salaries)
+//     for (const value of values) {
+//         totalSalary += value
+//     }
+//     return totalSalary;
+// }
+// console.log(countTotalSalary({}));
+// console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
+// console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }));
+
+
+
+// ###########################################################################################################################################################
+
+// // ==========================================Task-17
+
+// МАСИВ ОБ'ЄКТІВ
+// У стандартний набір повсякденних завдань розробника входить маніпуляція масивом однотипних об'єктів.
+// Це означає, що всі об'єкти в масиві гарантовано матимуть однаковий набір властивостей, але з різними значеннями.
+
+// const books = [
+//     {
+//         title: "The Last Kingdom",
+//         author: "Bernard Cornwell",
+//         rating: 8.38,
+//     },
+//     {
+//         title: "Beside Still Waters",
+//         author: "Robert Sheckley",
+//         rating: 8.51,
+//     },
+//     {
+//         title: "The Dream of a Ridiculous Man",
+//         author: "Fyodor Dostoevsky",
+//         rating: 7.75,
+//     },
+// ];
+// Для перебирання такого масиву використовується стандартний цикл for...of.
+// Значення властивостей кожного об'єкта можна отримати, використовуючи синтаксис «через крапку»,
+// оскільки в кожному об'єкті набір властивостей та їх імена будуть однакові, відрізняються тільки значення.
+
+//     for(const book of books) {
+//     // Об'єкт книги
+//     console.log(book);
+//     // Назва
+//     console.log(book.title);
+//     // Автор
+//     console.log(book.author);
+//     // Рейтинг
+//     console.log(book.rating);
+// }
+
+// //  --------------------===================== Сonditions =====================--------------------
+
+// Перебери масив об'єктів colors, використовуючи цикл for...of.
+// Додай у масив hexColors значення властивостей hex, а в масив rgbColors - значення властивостей rgb з усіх об'єктів масиву colors.
+
+// //  --------------------===================== Tests =====================--------------------
+
+// Оголошена змінна colors
+// Значення змінної colors - це масив
+// Оголошена змінна hexColors
+// Значення змінної hexColors - це масив["#f44336", "#2196f3", "#4caf50", "#ffeb3b"]
+// Оголошена змінна rgbColors
+// Значення змінної rgbColors - це масив["244,67,54", "33,150,243", "76,175,80", "255,235,59"]
+
+// //  --------------------===================== Result =====================--------------------
+
+// // Before
+
+// const colors = [
+//     { hex: "#f44336", rgb: "244,67,54" },
+//     { hex: "#2196f3", rgb: "33,150,243" },
+//     { hex: "#4caf50", rgb: "76,175,80" },
+//     { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+
+// // After
+
+// const colors = [
+//     { hex: "#f44336", rgb: "244,67,54" },
+//     { hex: "#2196f3", rgb: "33,150,243" },
+//     { hex: "#4caf50", rgb: "76,175,80" },
+//     { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+// const hexColors = [];
+// const rgbColors = [];
+// for (const color of colors) {
+//     hexColors.push(color.hex);
+//     rgbColors.push(color.rgb);
+// }
+
+
+
+// ###########################################################################################################################################################
+
+// // ==========================================Task-18
+// ЗАДАЧА.ПОШУК ОБ'ЄКТА ЗА ЗНАЧЕННЯМ ВЛАСТИВОСТІ
+
+// //  --------------------===================== Сonditions =====================--------------------
+
+// Напиши функцію getProductPrice(productName), яка приймає один параметр productName - назва продукту.
+// Функція шукає об'єкт продукту з таким ім'ям(властивість name) в масиві products і повертає його ціну(властивість price).
+// Якщо продукт з такою назвою не знайдений, функція повинна повертати null.
+
+// //  --------------------===================== Tests =====================--------------------
+
+// Оголошена функція getProductPrice(productName).
+// Виклик getProductPrice("Radar") повертає 1300.
+// Виклик getProductPrice("Grip") повертає 1200.
+// Виклик getProductPrice("Scanner") повертає 2700.
+// Виклик getProductPrice("Droid") повертає 400.
+// Виклик getProductPrice("Engine") повертає null.
+
+// //  --------------------===================== Result =====================--------------------
+
+// // Before
+
+// const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+//     // Change code above this line
+// }
+
+// // After
+
+// const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+//     for (const product of products) {
+//         if (product.name === productName) {
+//             return product.price;
+//         }
+//     }
+//     return null;
+// }
+
+
+
+// ###########################################################################################################################################################
+
+// // ==========================================Task-19
+// ЗАДАЧА: КОЛЕКЦІЯ ЗНАЧЕНЬ ВЛАСТИВОСТІ
+
+// //  --------------------===================== Сonditions =====================--------------------
+
+// Напиши функцію getAllPropValues(propName), яка приймає один параметр propName - ім'я (ключ) властивості.
+// Функція повинна повернути масив всіх значень властивості з таким ім'ям з кожного об'єкта в масиві products.
+// Якщо в об'єктах відсутні властивості з таким ім'ям, функція повинна повернути порожній масив.
+
+// //  --------------------===================== Tests =====================--------------------
+
+// Оголошена функція getAllPropValues(propName)
+// Виклик getAllPropValues("name") повертає["Radar", "Scanner", "Droid", "Grip"]
+// Виклик getAllPropValues("quantity") повертає[4, 3, 7, 9]
+// Виклик getAllPropValues("price") повертає[1300, 2700, 400, 1200]
+// Виклик getAllPropValues("category") повертає[]
+
+// //  --------------------===================== Result =====================--------------------
+
+// // Before
+
+// const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+//     // Change code below this line
+// }
+
+// // After
+
+// const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+//     let allNames = [];
+//     for (let product of products) {
+//         if (propName === "name") {
+//             allNames.push(product.name);
+//         }
+//         if (propName === "quantity") {
+//             allNames.push(product.quantity);
+//         }
+//         if (propName === "price") {
+//             allNames.push(product.price);
+//         } else {
+//             allNames.push();
+//         }
+//          console.log(product.name);
+//     }
+//     return allNames;
+// }
+
+
+
+// ###########################################################################################################################################################
+
+// // ==========================================Task-20
+
 // //  --------------------===================== Сonditions =====================--------------------
 
 // //  --------------------===================== Tests =====================--------------------
@@ -1100,6 +1415,16 @@
 
 
 // // After
+
+
+
+
+
+
+
+
+
+
 
 
 
