fa77af0ca2a877210851bdc31587d4fc21b4fcc1
Rodion Shpak <98263512+RodionShpak@users.noreply.github.com>
2022-07-30 14:49:55 +0300

    slug

--- goit_js/repeta_theory/mod_2_part1.js
+++ goit_js/repeta_theory/mod_2_part1.js
@@ -111,10 +111,10 @@
 
 
 // ------------------ Задача ------------------ //
-
 /*
 * напиши скрипт который подсчитывает сумму всех чётных чисел в массиве.
 */
+
 // const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 11];
 // // 1 переменная тотал
 // let total = 0;
@@ -141,14 +141,236 @@
 // }
 // console.log('Total: ', total);
 
+// ------------------ или через continue (от обратного) ------------------ //
+
+// for (const number of numbers) {
+//     if (number % 2 !== 0) {
+//         console.log('Эту итерацию пропустить!!!', number);
+//         continue;
+//     }
+//     console.log(`${number} - чётное!!!`);
+// total += number;
+// }
+// console.log('Total: ', total);
+
 
 
 // ------------------ Задача ------------------ //
 
+/*
+* Напиши скрипт поиска логина
+*   - Если логина нет, вывести сообщение 'Пользователь [логин]' не найден'
+*   - Если нашли логин, вывести сообщение 'Пользователь [логин]' найден'
+*
+* - Сначала через for
+* - Потом через for...of
+* - Логика break
+* - Метод includes() c тернарным оператором
+*/
+
+// const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'aj4xth3m4n'];
+// const loginToFind = 'poly1scute';
+// let message = '';
+
+// * - Сначала через for - * //
+
+// for (let i = 0; i < logins.length; i += 1) {
+//     const login = logins[i];
+//
+// if (login !== loginToFind) {
+//         message = `Пользователь ${loginToFind} не найден.`;
+//     } else {
+//         message = `Пользователь ${loginToFind} найден.`;
+//     }
+// }                         цикл не прерывается по этому не ищет
+// console.log(message);  // Пользователь poly1scute не найден.
+
+// * - сделаем так что бы цикл прерывался через break - * //
+// const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'aj4xth3m4n'];
+// const loginToFind = 'poly1scute';
+// let message = '';
+
+// for (let i = 0; i < logins.length; i += 1) {
+//     const login = logins[i];
+//
+// if (login === loginToFind) {
+//         message = `Пользователь ${loginToFind} найден.`;
+//         break;
+//     }
+//     message = `Пользователь ${loginToFind} не найден.`;
+// }
+// console.log(message); // Пользователь poly1scute найден.
+
+// * - cравниваем - * //
+
+// const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'aj4xth3m4n'];
+// const loginToFind = 'poly1scute';
+// let message = '';
+
+// for (let i = 0; i < logins.length; i += 1) {
+//     const login = logins[i];
+//     console.log('Login: ', login);
+//     console.log(`${login} === ${loginToFind}: `, login === loginToFind);
+//
+// if (login === loginToFind) {
+//         console.log('Ура, равны!!!');
+//         message = `Пользователь ${loginToFind} найден.`;
+//         break;
+//     }
+//     message = `Пользователь ${loginToFind} не найден.`;
+// }
+// console.log(message); // Пользователь poly1scute найден.
+
+// * - сделаем по короче - * //
+// const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'aj4xth3m4n'];
+// const loginToFind = 'poly1scute';
+// let message = 'Пользователь ${loginToFind} не найден.';
+
+// for (let i = 0; i < logins.length; i += 1) {
+//     const login = logins[i];
+//     console.log('Login: ', login);
+//     console.log(`${login} === ${loginToFind}: `, login === loginToFind);
+//
+// if (login === loginToFind) {
+//         console.log('Ура, равны!!!');
+//         message = `Пользователь ${loginToFind} найден.`;
+//         break;
+//     }
+// }
+// console.log(message); // Пользователь poly1scute найден.
+
+// * - Потом через for...of - * //
+
+// const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'aj4xth3m4n'];
+// const loginToFind = 'poly1scute';
+// let message = 'Пользователь ${loginToFind} не найден.';
+
+// for (const login of logins) {
+//     console.log('Login: ', login);
+//     console.log(`${login} === ${loginToFind}: `, login === loginToFind);
+
+//     if (login === loginToFind) {
+//         console.log('Ура, равны!!!');
+//         message = `Пользователь ${loginToFind} найден.`;
+//         break;
+//     }
+// }
+// console.log(message); // Пользователь poly1scute найден.
+
+// * - Метод includes() c тернарным оператором - *//
+
+// const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'aj4xth3m4n'];
+// const loginToFind = 'poly1scute';
+// // let message = 'Пользователь ${loginToFind} не найден.';
+
+// const message = logins.includes(loginToFind)
+//     ? `Пользователь ${loginToFind} найден.`
+//     : `Пользователь ${loginToFind} не найден.`;
+
+// console.log(message); // Пользователь poly1scute найден.
 
 
 
+// ------------------ Задача for...of ------------------ //
+
+/*
+* Напиши скрипт поиска самого маленького числа в массиве,
+* при условии что числа уникальные (не повторяются).
+*/
+
+// const numbers = [51, 18, 13, 24, 7, 85, 19];
+// let smallesNumber = numbers[0];
+
+// for (const number of numbers) {
+//     if (number < smallesNumber) {
+//         smallesNumber = number;
+//     }
+// }
+// console.log('smallesNumber: ', smallesNumber); // smallesNumber: 7
 
 
+
+// ------------------ Задача на метод join ------------------ //
+/*
+* Напиши скрипт, который объеденяет все элементы массива в одно строковое значение.
+* Элементов может быть произвольное кол-во.
+* Пусть элементы массива в строке будут разделены запятой.
+* - Сначала через for
+* - Потом через join()
+* // Должно получится 'Mango,Poly,Kiwi,Ajax'
+*/
+
+// * - Сначала через for - * //
+
+// const friends = ['Mango', 'Poly', 'Kiwi', 'Ajax'];
+// let string = '';
+// for (const friend of friends) {
+//     string += friend + ',';
+// }
+// string = string.slice(0, string.length - 1);
+// console.log(string); // Mango,Poly,Kiwi,Ajax
+
+// * - Потом через join() - * //
+
+// const friends = ['Mango', 'Poly', 'Kiwi', 'Ajax'];
+
+// const string = friends.join(', ');
+// console.log(string); // Mango, Poly, Kiwi, Ajax
+
+
+
+// ------------------ Задача на регистр и тернарый оператор ------------------ //
+/*
+* Напиши скрипт который заменяет регистр каждого символа в строке на противоположный.
+* Например, если строка "JavaScript", то на выходе должна быть строка "jAVAsCRIPT".
+*/
+
+// const string = 'JavaScript';
+// const letters = string.split('');
+// // console.log(letters); // 'J', 'a', 'v', 'a', 'S', 'c', 'r', 'i', 'p', 't'
+// let invertedString = '';
+
+// for (const letter of letters) {
+//     console.log(letter);
+
+// if (letter === letter.toLowerCase()) {
+//     console.log('Эта буква в нижнем регистре!!! -', letter);
+//     invertedString += letter.toUpperCase();
+//     // console.log(invertedString); // AVACRIPT
+// } else {
+//     invertedString += letter.toLowerCase();
+//     // console.log(invertedString); // jAVAsCRIPT
+// }
+
+// * - через тернарный оператор - * //
+
+// invertedString +=
+//     letter === letter.toLowerCase()
+//         ? letter.toUpperCase()
+//         : letter.toLowerCase();
+// }
+// console.log(invertedString); // jAVAsCRIPT
+
+
+
+// ------------------ Задача  ------------------ //
+/*
+* Делаем slug в URL из названия статьи (например на dev.to)
+* Заголовок статьи состоит только из букв и пробелов
+*
+* - нормализируем строку
+* - разбиваем по словам
+* - сшиваем в строку с разделителями
+*/
+
+// Должно получиться top-10-benefits-of-react-framework
+
+const title = 'Top 10 benefits of React framework';
+const normalizedTitle = title.toLowerCase();
+// console.log(normalizedTitle); // top 10 benefits of react framework
+const words = normalizedTitle.split(' ');
+// console.log(words); // ['top', '10', 'benefits', 'of', 'react', 'framework']
+const slug = words.join('-');
+// console.log(slug); // top-10-benefits-of-react-framework
 
 
