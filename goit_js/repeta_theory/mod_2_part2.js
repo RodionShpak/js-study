e971f59e18290622bd3bc19791b3e896e94c2947
Rodion Shpak <98263512+RodionShpak@users.noreply.github.com>
2022-08-01 22:00:56 +0300

    cmvn

--- goit_js/repeta_theory/mod_2_part2.js
+++ goit_js/repeta_theory/mod_2_part2.js
@@ -43,32 +43,83 @@
 
 // console.log('Результат функции: ', fn(1000)); // Результат функции:  Больше чем 50
 
-
 /*
 *   - Стек вызовов
 *   - Stack trace и поиск ошибок
 */
 
-const fnA = function () {
-    console.log('Выполняется функция А');
-};
+// const fnA = function () {
+//     console.log('Выполняется функция А');
+// };
 
-const fnB = function () {
-    console.log('Выполняется функция B');
-};
+// const fnB = function () {
+//     console.log('Выполняется функция B');
+// };
 
-const fnC = function () {
-    console.log('Выполняется функция C');
-};
+// const fnC = function () {
+//     console.log('Выполняется функция C');
+// };
 
-// console.log('Лог перед вызовом функции А');
-fnA();
-// console.log('Лог после вызова функции А');
+// // console.log('Лог перед вызовом функции А');
+// fnA();
+// // console.log('Лог после вызова функции А');
 
-// console.log('Лог перед вызовом функции B');
-fnB();
-// console.log('Лог после вызова функции B');
+// // console.log('Лог перед вызовом функции B');
+// fnB();
+// // console.log('Лог после вызова функции B');
 
-// console.log('Лог перед вызовом функции C');
-fnC();
-// console.log('Лог после вызова функции C');
+// // console.log('Лог перед вызовом функции C');
+// fnC();
+// // console.log('Лог после вызова функции C');
+
+
+
+// ------------------ Задача ------------------ //
+/*
+* Напиши функцию calculateTotalPrice(items)
+* которая принимает массив цен (чисел) и возвращает их сумму
+*/
+
+// const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
+// let total = 0;
+
+// for (const value of cart) {
+//     total += value;
+// }
+
+// console.log('Total: ', total); // Total: 613
+
+const calculateTotalPrice = function (items) {
+    console.log(items);
+
+    let total = 0;
+
+    for (const item of items) {
+        total += item;
+    }
+
+    return total;
+}
+
+console.log(calculateTotalPrice([1, 2, 3])); // 6
+console.log(calculateTotalPrice([5, 10, 15, 20])); // 50
+console.log(calculateTotalPrice([100, 200, 300])); // 600
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
