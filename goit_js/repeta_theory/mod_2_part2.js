daec9c862916da469a7e7dcd7b5dcf271edca569
Rodion Shpak <98263512+RodionShpak@users.noreply.github.com>
2022-08-02 19:37:26 +0300

    моп

--- goit_js/repeta_theory/mod_2_part2.js
+++ goit_js/repeta_theory/mod_2_part2.js
@@ -196,5 +196,25 @@
 
 
 // ------------------ Задача ------------------ //
+/*
+* Напиши функцию findSmallesNumber(numbers) для поиска самого маленького числа в массиве,
+* при условии что числа уникальные (не повторяются).
+*/
 
+const numbers = [51, 18, 13, 24, 7, 85, 19];
+let smallestNumber = numbers[0];
+
+for (const number of numbers) {
+    if (number < smallestNumber) {
+        smallestNumber = number;
+    }
+}
+
+console.log('smallestNumber: ', smallestNumber);
+
+
+
+console.log(findSmallesNumber([3, 8, 12, -2, 15])); // -2
+console.log(findSmallesNumber([100, 54, 8, 12, 47])); // 8
+console.log(findSmallesNumber([7, 21, 84, 15, 4])); // 4
 
