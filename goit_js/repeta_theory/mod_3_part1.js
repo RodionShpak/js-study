9639f56a1e8b2a006ce5e713bde658e70f9434af
Rodion Shpak <98263512+RodionShpak@users.noreply.github.com>
2022-08-03 13:21:00 +0300

    frgjg

---
+++ goit_js/repeta_theory/mod_3_part1.js
@@ -0,0 +1,203 @@
+/*
+ * Объекты (делаем плейлист музыки: имя, рейтинг, треки, кол-во треков)
+ * - Литерал объекта
+ * - Свойства, ключи (имя) и значения
+ * - Как отличить объект от области видимости
+ */
+
+
+// const playlist = {  // {} - литерал обьекта
+//     name: [1, 2, 3],  // name - ключ // [] - свойства (значение) обьекта
+//     b: 5,   // b: - ключ // 5 - свойство
+//     c: 10  // с: - ключ // 10 - свойство
+// };
+
+///************* Как отличить объект от области видимости **************///
+// const x = {};
+
+// const fn = function (myObject) {
+//     // myObject = {a: 1, b: 2}
+//     console.log(myObject);
+// };
+
+// fn({ a: 1, b: 2 });
+
+///************* Как отличить объект от области видимости **************///
+
+// const rtfm = function () {
+//     return { a: 5 };
+// };
+
+// console.log(rtfm());
+
+/* Доступ к свойству
+//     * - obj.key
+//     * - obj['key']
+//     * - Отсутствующие свойства
+*/
+
+const playlist = {
+    name: 'Мой супер плейлист',
+    rating: 5,
+    tracks: ['трек-1', 'трек-2', 'трек-3'],
+    trackCount: 3,
+};
+
+//****** ключ свойства всегда строка ******//
+console.log(playlist.name); // получаем доступ через ключ к значению
+console.log(playlist.tracks);  // ['трек-1', 'трек-2', 'трек-3']
+console.log(playlist.trackCount);  // 3
+
+////****** имя свойства хранится в переменной ******////
+
+const propertyName = 'tracks';
+
+// console.log(playlist.propertyName);  // undefined
+
+//****** как достучаться до свойства ******//
+// console.log(playlist.rating);  // 5
+// console.log(playlist['rating']);  // 5
+
+console.log(playlist.propertyName); // undefined
+console.log(playlist[propertyName]); // ['трек-1', 'трек-2', 'трек-3']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // const playlist = {
+// //   name: 'Мой супер плейлист',
+// //   rating: 5,
+// //   tracks: ['трек-1', 'трек-2', 'трек-3'],
+// //   trackCount: 3,
+// // };
+
+// // console.log(playlist);
+
+// // const x = {};
+
+// // const fn = function (myObject) {
+// //   // myObject = { a: 1, b: 2 }
+// //   console.log(myObject);
+// // };
+
+// // fn({ a: 1, b: 2 });
+
+// // const rtfm = function () {
+// //   return { a: 5 };
+// // };
+
+// // console.log(rtfm());
+
+// /*
+
+
+// const playlist = {
+//     name: 'Мой супер плейлист',
+//     rating: 5,
+//     tracks: ['трек-1', 'трек-2', 'трек-3'],
+//     trackCount: 3,
+// };
+
+// // playlist.qwe = 5;
+
+// // playlist.rating = 10;
+
+// // console.log(playlist);
+
+// // console.log(playlist);
+// // console.log(playlist.tracks);
+// // console.log(playlist.name);
+// // console.log(playlist.trackCount);
+
+// const propertyName = 'tracks';
+
+// // console.log(playlist.rating);
+// // console.log(playlist['rating']);
+
+// // console.log(playlist.propertyName);
+// // console.log(playlist[propertyName]);
+
+// /*
+//  * Короткая запись свойств
+//  */
+// const username = 'Mango';
+// const email = 'mango@mail.com';
+
+// const signupData = {
+//     username,
+//     email,
+// };
+
+// // console.log(signupData);
+
+// /*
+//  * Вычисляемые свойства
+//  */
+
+// //  <input name="color" value="tomato" >
+
+// const inputName = 'color';
+// const inputValue = 'tomato';
+
+// const colorPickerData = {
+//     [inputName]: inputValue,
+// };
+
+// // console.log(colorPickerData);
+
+// /*
+//  * Ссылочный тип {} === {}
+//  */
+
+// // console.log({ a: 1 } === { a: 1 });
+// // console.log([] === []);
+
+// // const a = { x: 1, y: 2 };
+// // const b = a;
+
+// // console.log(b === a);
+
+// // a.hello = 100;
+// // b.hello = 150;
+
+// // console.log(a);
+// // console.log(b);
+
+// /*
+//  * Массивы и функции это объекты
+//  */
+
+// // const a = [1, 2, 3];
+
+// // a.hello = ':)';
+
+// // console.log(a);
+
+// // const fn = function () {
+// //   console.log('hello');
+// // };
+
+// // fn.hello = ';)';
+
+// // console.dir(fn.hello);
+
+
+
+// ------------------  ------------------ //
