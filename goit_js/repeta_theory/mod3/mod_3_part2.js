08b22e5b818afee7842c2f93f2be5029c2ffeb5a
Rodion Shpak <98263512+RodionShpak@users.noreply.github.com>
2022-08-18 12:50:58 +0300

    d

--- goit_js/repeta_theory/mod3/mod_3_part2.js
+++ goit_js/repeta_theory/mod3/mod_3_part2.js
@@ -289,17 +289,3 @@ const profile = {
 };
 
 showProfileInfo(profile);
-
-
-
-function count(countFrom = 0, countTo = 10, step = 1) {
-    console.log(`countFrom = ${countFrom}, countTo = ${countTo}, step = ${step}`);
-
-    for (let i = countFrom; i <= countTo; i += step) {
-        console.log(i);
-    }
-}
-
-count(1, 5); // countFrom = 1, countTo = 5, step = 1
-count(2); // countFrom = 2, countTo = 10, step = 1
-count(); // countFrom = 0, countTo = 10, step = 1
